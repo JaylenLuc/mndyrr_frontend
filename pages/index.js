68075bd4d23444a329a9b93cc18f124216888d17
JaylenLuc <JHLUC@UCI.EDU>
2024-05-15 16:12:29 -0700

    firdt

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+"use client"
+import Head from 'next/head';
+import Link from 'next/link';
+import Chat from '../mendyChat/chat';
+
+export default function Home() {
+  return (
+
+      <Chat></Chat>
+
+  );
+}
